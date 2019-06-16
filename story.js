404f3c8fa8c141d74c04198e552c9b23e36d8f47
qtip <qtipkk@gmail.com>
2019-06-15 17:56:38 -0700

    Add the rest of the personal story

--- story.js
+++ story.js
@@ -80,54 +80,54 @@ export default function renderStoryFlow(b) {
                 ])
             ]),
             b.vbox("Norn - Forging a Legend", [
-                b.quest(0),
+                b.quest(154),
                 b.hbox(null, [
                     b.vbox("Defeat Our Ancient Foes", [
-                        b.quest(0),
-                        b.quest(0),
-                        b.quest(0),
-                        b.hbox(null, [b.quest(0), b.quest(0)]),
-                        b.quest(0),
+                        b.quest(155),
+                        b.quest(157),
+                        b.quest(158),
+                        b.hbox(null, [b.quest(160), b.quest(161)]),
+                        b.quest(156),
                     ]),
                     b.vbox("Protect the Spirits", [
-                        b.quest(0),
-                        b.quest(0),
-                        b.quest(0),
-                        b.hbox(null, [b.quest(0), b.quest(0)]),
-                        b.quest(0)
+                        b.quest(159),
+                        b.quest(163),
+                        b.quest(165),
+                        b.hbox(null, [b.quest(167), b.quest(166)]),
+                        b.quest(172)
                     ]),
                     b.vbox("Guard the Mists", [
-                        b.quest(0),
-                        b.quest(0),
-                        b.quest(0),
-                        b.hbox(null, [b.quest(0), b.quest(0)]),
-                        b.quest(0),
+                        b.quest(162),
+                        b.quest(164),
+                        b.hbox(null, [b.quest(169), b.quest(168)]),
+                        b.quest(170),
+                        b.quest(171)
                     ]),
                 ])
             ]),
             b.vbox("Sylvari - Following the Dream", [
-                b.quest(0),
+                b.quest(201),
                 b.hbox(null, [
                     b.vbox("White Stag", [
-                        b.quest(0),
-                        b.quest(0),
-                        b.quest(0),
-                        b.hbox(null, [b.quest(0), b.quest(0)]),
-                        b.quest(0),
+                        b.quest(202),
+                        b.quest(206),
+                        b.quest(204),
+                        b.hbox(null, [b.quest(210), b.quest(207)]),
+                        b.quest(209),
                     ]),
                     b.vbox("Green Huntsman", [
-                        b.quest(0),
-                        b.quest(0),
-                        b.quest(0),
-                        b.hbox(null, [b.quest(0), b.quest(0)]),
-                        b.quest(0)
+                        b.quest(203),
+                        b.quest(205),
+                        b.quest(208),
+                        b.hbox(null, [b.quest(218), b.quest(211)]),
+                        b.quest(219)
                     ]),
                     b.vbox("Shield of the Moon", [
-                        b.quest(0),
-                        b.quest(0),
-                        b.quest(0),
-                        b.hbox(null, [b.quest(0), b.quest(0)]),
-                        b.quest(0),
+                        b.quest(212),
+                        b.quest(213),
+                        b.quest(214),
+                        b.quest(215),
+                        b.hbox(null, [b.quest(217), b.quest(216)]),
                     ]),
                 ])
             ]),
@@ -135,48 +135,42 @@ export default function renderStoryFlow(b) {
         b.hbox("Part 2", [
             b.hbox("Asura - Interested Parties", [
                 b.vbox("VAL-A Golem", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)]),
-                    b.quest(0),
+                    b.quest(39),
+                    b.quest(40),
+                    b.hbox(null, [b.quest(41), b.quest(43)]),
+                    b.quest(42),
                 ]),
                 b.vbox("Transatmospheric Converter", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)]),
-                    b.quest(0),
+                    b.quest(44),
+                    b.quest(45),
+                    b.hbox(null, [b.quest(46), b.quest(48)]),
+                    b.quest(47),
                 ]),
                 b.vbox("Infinity Ball", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)]),
-                    b.quest(0),
+                    b.quest(34),
+                    b.quest(35),
+                    b.hbox(null, [b.quest(36), b.quest(37)]),
+                    b.quest(38),
                 ]),
             ]),
             b.hbox("Charr - Sins of the Father", [
                 b.vbox("Loyal Soldier", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)]),
-                    b.quest(0),
-                    b.quest(0)
+                    b.quest(78),
+                    b.quest(79),
+                    b.hbox(null, [b.quest(81), b.quest(80)]),
+                    b.quest(82)
                 ]),
                 b.vbox("Honorless Gladium", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)]),
-                    b.quest(0),
-                    b.quest(0)
+                    b.quest(83),
+                    b.quest(84),
+                    b.quest(85),
+                    b.hbox(null, [b.quest(86), b.quest(87)]),
                 ]),
                 b.vbox("Sorcerous Shaman", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)])
+                    b.quest(88),
+                    b.quest(89),
+                    b.quest(90),
+                    b.hbox(null, [b.quest(91), b.quest(92)])
                 ]),
             ]),
             b.hbox("Human - Shadows of the Past", [
@@ -201,50 +195,217 @@ export default function renderStoryFlow(b) {
             ]),
             b.hbox("Norn - Everyone Makes Mistakes", [
                 b.vbox("Blacked Out", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)]),
-                    b.quest(0),
+                    b.quest(173),
+                    b.quest(174),
+                    b.hbox(null, [b.quest(182), b.quest(176)]),
+                    b.quest(181),
                 ]),
                 b.vbox("Revenge", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)]),
-                    b.quest(0)
+                    b.quest(175),
+                    b.hbox(null, [b.quest(177), b.quest(178)]),
+                    b.quest(179),
+                    b.quest(180),
                 ]),
                 b.vbox("Lost an Heirloom", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)]),
-                    b.quest(0),
+                    b.quest(184),
+                    b.quest(183),
+                    b.quest(185),
+                    b.hbox(null, [b.quest(186), b.quest(187)]),
                 ]),
             ]),
             b.hbox("Sylvari - The Teachings of Ventari", [
                 b.vbox("Act With Wisdom", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)]),
-                    b.quest(0),
+                    b.quest(230),
+                    b.quest(231),
+                    b.quest(232),
+                    b.hbox(null, [b.quest(233), b.quest(234)]),
                 ]),
                 b.vbox("All things have a right to grow", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)]),
-                    b.quest(0)
+                    b.quest(220),
+                    b.quest(221),
+                    b.hbox(null, [b.quest(222), b.quest(223)]),
+                    b.quest(224),
                 ]),
                 b.vbox("Where Life Goes", [
-                    b.quest(0),
-                    b.quest(0),
-                    b.quest(0),
-                    b.hbox(null, [b.quest(0), b.quest(0)]),
-                    b.quest(0),
+                    b.quest(225),
+                    b.quest(226),
+                    b.hbox(null, [b.quest(227), b.quest(228)]),
+                    b.quest(229),
                 ]),
             ]),
+        ]),
+        b.hbox("Part 3", [
+            b.chapter(8, 2, [
+                    b.quest(49),
+                    b.hbox(null, [b.quest(50), b.quest(51)]),
+                    b.hbox(null, [b.quest(56), b.quest(57)]),
+                    b.hbox(null, [b.quest(52), b.quest(53)]),
+                    b.hbox(null, [b.quest(54), b.quest(55), b.quest(58)]),
+            ]),
+            b.chapter(1, 2, [
+                    b.quest(93),
+                    b.hbox(null, [b.quest(94), b.quest(95)]),
+                    b.hbox(null, [b.quest(97), b.quest(98)]),
+                    b.hbox(null, [b.quest(100), b.quest(101)]),
+                    b.hbox(null, [b.quest(104), b.quest(105), b.quest(102)]),
+            ]),
+            b.chapter(3, 2, [
+                    b.quest(140),
+                    b.hbox(null, [b.quest(141), b.quest(142)]),
+                    b.hbox(null, [b.quest(145), b.quest(144)]),
+                    b.hbox(null, [b.quest(147), b.quest(148)]),
+                    b.hbox(null, [b.quest(151), b.quest(152), b.quest(150)]),
+            ]),
+            b.chapter(2, 2, [
+                    b.quest(188),
+                    b.hbox(null, [b.quest(189), b.quest(197)]),
+                    b.hbox(null, [b.quest(194), b.quest(191)]),
+                    b.hbox(null, [b.quest(192), b.quest(195)]),
+                    b.hbox(null, [b.quest(200), b.quest(199), b.quest(198)]),
+            ]),
+            b.chapter(7, 2, [
+                    b.quest(236),
+                    b.hbox(null, [b.quest(237), b.quest(239)]),
+                    b.hbox(null, [b.quest(244), b.quest(238)]),
+                    b.hbox(null, [b.quest(235), b.quest(241)]),
+                    b.hbox(null, [b.quest(247), b.quest(243), b.quest(246)]),
+            ]),
+        ]),
+        b.chapter(9, 0, [
+            b.quest(358),
+            b.hbox(null, [
+                b.vbox("Durmand Priory", [
+                    b.quest(251),
+                    b.quest(256),
+                    b.hbox(null, [b.quest(261), b.quest(254)]),
+                    b.quest(257),
+                ]),
+                b.vbox("Order of Whispers", [
+                    b.quest(253),
+                    b.quest(255),
+                    b.hbox(null, [b.quest(258), b.quest(259)]),
+                    b.quest(260),
+                ]),
+                b.vbox("Vigil", [
+                    b.quest(248),
+                    b.quest(249),
+                    b.quest(250),
+                    b.hbox(null, [b.quest(262), b.quest(252)]),
+                ]),
+            ])
+        ]),
+        b.chapter(9, 1, [
+            b.hbox(null, [
+                b.vbox("Grawl", [
+                    b.quest(263),
+                    b.hbox(null, [b.quest(264), b.quest(266)]),
+                    b.quest(265),
+                ]),
+                b.vbox("Hylek", [
+                    b.quest(267),
+                    b.hbox(null, [b.quest(268), b.quest(270)]),
+                    b.quest(269),
+                ]),
+                b.vbox("Ogre", [
+                    b.quest(271),
+                    b.quest(272),
+                    b.quest(273),
+                ]),
+                b.vbox("Quaggan", [
+                    b.quest(274),
+                    b.hbox(null, [b.quest(275), b.quest(276)]),
+                    b.quest(277),
+                ]),
+                b.vbox("Skritt", [
+                    b.quest(278),
+                    b.hbox(null, [b.quest(279), b.quest(280)]),
+                    b.quest(281),
+                ]),
+            ])
+        ]),
+        b.chapter(9, 2, [
+            b.hbox(null, [
+                b.vbox("Durmand Priory", [b.quest(285)]),
+                b.vbox("Order of Whispers", [b.quest(290)]),
+                b.vbox("Vigil", [b.quest(527)]),
+            ]),
+            b.quest(282),
+            b.hbox(null, [
+                b.vbox("Durmand Priory", [
+                    b.quest(286),
+                    b.hbox(null, [b.quest(288), b.quest(287)]),
+                ]),
+                b.vbox("Order of Whispers", [
+                    b.quest(291),
+                    b.hbox(null, [b.quest(292), b.quest(293)]),
+                ]),
+                b.vbox("Vigil", [
+                    b.quest(296),
+                    b.hbox(null, [b.quest(298), b.quest(297)]),
+                ]),
+            ]),
+            b.quest(283),
+            b.hbox(null, [
+                b.vbox("Durmand Priory", [b.quest(289)]),
+                b.vbox("Order of Whispers", [b.quest(294)]),
+                b.vbox("Vigil", [b.quest(299)]),
+            ]),
+            b.quest(284),
+        ]),
+        b.chapter(10, 0, [
+            b.quest(301),
+            b.hbox(null, [
+                b.vbox("Let an Innocent Die", [
+                    b.quest(309),
+                    b.hbox(null, [b.quest(307), b.quest(308)]),
+                    b.quest(310),
+                ]),
+                b.vbox("Dishonor My Allies", [
+                    b.quest(304),
+                    b.hbox(null, [b.quest(303), b.quest(302)]),
+                    b.quest(305),
+                ]),
+                b.vbox("Make Another Suffer", [
+                    b.quest(312),
+                    b.quest(313),
+                    b.hbox(null, [b.quest(314), b.quest(315)]),
+                ]),
+            ]),
+            b.quest(300),
+            b.hbox(null, [
+                b.vbox("Let an Innocent Die", [b.quest(311)]),
+                b.vbox("Dishonor My Allies", [b.quest(306)]),
+                b.vbox("Make Another Suffer", [b.quest(316)]),
+            ]),
+        ]),
+        b.chapter(10, 1, [
+            b.quest(321),
+            b.hbox(null, [b.quest(335), b.quest(334)]),
+            b.quest(320),
+            b.hbox(null, [b.quest(337), b.quest(336)]),
+            b.hbox(null, [b.quest(339), b.quest(338)]),
+            b.quest(318),
+            b.hbox(null, [
+                b.vbox("Priory's Plan", [
+                    b.quest(325),
+                    b.quest(326),
+                    b.quest(327),
+                ]),
+                b.vbox("Whispers' Plan", [
+                    b.quest(331),
+                    b.quest(332),
+                    b.quest(333),
+                ]),
+                b.vbox("Vigil's Plan", [
+                    b.quest(328),
+                    b.quest(329),
+                    b.quest(330),
+                ]),
+            ]),
+            b.quest(322),
+            b.hbox(null, [b.quest(317), b.quest(319)]),
+            b.quest(323),
+            b.quest(324),
         ])
     ]);
 }
